ffbf34c88f509e1132f57fb433c668a1be2b5615
Daniel Winterstein <daniel@good-loop.com>
2023-06-09 14:40:49 +0100

    why-has-it-stopped-working

--- builder/BuildGoose.java
+++ builder/BuildGoose.java
@@ -22,11 +22,22 @@ public class BuildGoose extends BuildWinterwellProject {
 		mdt.addDependency("com.google.guava", "guava", "28.2-jre");
 		
 		String jversion = "10.0.15";
+		String jakarta = "1.1.2";
+		
+      mdt.addDependency("jakarta.websocket:jakarta.websocket-api:"+jakarta);
+//	    <!-- To run javax.websocket in embedded server -->
+      mdt.addDependency("org.eclipse.jetty.websocket:websocket-javax-server:"+jversion);
+//	    <!-- To run javax.websocket client -->
+      mdt.addDependency("org.eclipse.jetty.websocket:websocket-javax-client:"+jversion);
+
+		
 		mdt.addDependency("org.eclipse.jetty.websocket:websocket-jetty-api:"+jversion);
 		mdt.addDependency("org.eclipse.jetty.websocket:websocket-jetty-server:"+jversion);
 //		mdt.addDependency("org.eclipse.jetty.websocket:websocket-server:"+jversion); // version??
 		
+//		mdt.run();		
 		deps.add(mdt);
+		
 		return deps;
 	}
 	
